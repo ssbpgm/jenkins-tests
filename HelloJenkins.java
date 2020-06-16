2499ac0e1f1502ec07c12e0ad0716b50a0ef3160
Pedro García Medina <pagm.tech@gmail.com>
2020-06-16 15:33:44 -0500

    Update HelloJenkins.java
    trying a trigger of a jenkins job by a github webhook

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -4,6 +4,7 @@ public class HelloJenkins {
 	public static void main(String[] args) {
 		System.out.println("Hello, Jenkins, this is the Octocat from GitHub!");
 		System.out.println("... some added text");
+		System.out.println("did the webhook work?");
 	}
 	
 }
